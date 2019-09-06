8d882524c6d9bd28673a19b58618a12c8a0a08fe
AbdelIdir <homeworkoracle@protonmail.com>
2019-09-06 12:31:27 +0200

    cleaner code

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -14,43 +14,26 @@ function tabMaker(data){
  const tab = document.createElement("div");
     tab.classList.add("tab");
     topics.appendChild(tab);
-
-
     tab.textContent = data;
-
-    
+  
     return tab;
 }
-//topics.appendChild(tabMaker());
 const topics = document.querySelector(".topics");
-//topics.appendChild(tabMaker());
+
 
 
 axios.get("https://lambda-times-backend.herokuapp.com/topics")
 
-//axios.get('https://api.github.com/users/Jobsy')
 .then(response => {
   console.log(response)
   const res = response.data.topics;
   res.unshift("all");
   console.log(res);
   res.forEach(element => {
-
-
       tabMaker(element);
-
-      
   });
-  
-  
 })
 .catch(error => {
  console.error(error)
 });
 
-// }
-// const listFollowers = followersArray.map(function ( e,index, arr) {
-//   follower(arr[index])
-// });
-
-// response.data.topics.forEach(people => tab.peoples);
